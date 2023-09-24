a4c19e4aa7e93d5fb707e35ede8596077ff6a85e
Its-AJHarsh <122255411+Its-AJHarsh@users.noreply.github.com>
2023-09-24 20:47:52 +0530

    First commit
    ID: 1286
    
    Problem:
    The writers, operate independently and do not have a centralized platform to offer their services to clients. Objective:  to develop an incentivized design to onboard legal service providers on an eMarketplace to extend legal services to citizens in India.
    
    Expected to design a platform that can onboard legal service providers on an eMarketplace and incentivize them to offer their services to clients. The platform should be user-friendly,
    
    should address the following challenges:
    1. How to incentivize legal service providers to offer their services on the platform?
    2. How to ensure transparency and accountability in the services provided by legal service providers?
    3. How to ensure the quality of legal services provided by the service providers?
    4. How to ensure that clients can easily find the right legal service provider for their specific legal needs?
    5. How to ensure that the platform is accessible to citizens from all socio-economic backgrounds?
    
    Participants can consider various models for incentivizing legal service providers, such as offering them bonuses, rewards, or other forms of recognition for their services. They can also consider gamification techniques to motivate providers to offer quality services and compete with each other on the platform.
    
    Additionally, they can explore ways to integrate the platform with other legal service providers and institutions, such as courts, legal aid clinics, and bar associations.
    
    Deliverables: The deliverables for this hackathon are a functional prototype of the e-marketplace platform, including wireframes, user stories, and a brief video demonstration of the platform's features and functionalities. The platform should be designed to address the needs of both legal service providers and clients and should offer a seamless user experience.
    
    <!--Ministry of Law and Justice-->

---
+++ api/index.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const path = require("path");
+
+dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("File upload failed");
+  }
+});
+
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
+// Error handling middleware
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).json({ message: "Something went wrong" });
+});
+
+// Start the server
+const port = process.env.PORT || 8800;
+app.listen(port, () => {
+  console.log(`Backend server is running on port ${port}`);
+});
