b616b2fe3ea515e62e0b87969f7fcabc629a1529
AJ Harsh Vardhan <122255411+ItsAJ1005@users.noreply.github.com>
2023-09-25 17:07:42 +0530

    minor bug in login page.

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -21,9 +21,9 @@ export default function Login() {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">Lamasocial</h3>
+          <h3 className="loginLogo">inLaw</h3>
           <span className="loginDesc">
-            Connect with friends and the world around you on Lamasocial.
+            Connect with friends and the world around you on inLaw.
           </span>
         </div>
         <div className="loginRight">
